ddedef9fee166b60d2bad202debe2b7712795892
Andrew Stone <stoand01@ecolecatholique.ca>
2020-12-04 15:20:46 +0000

    update

--- sommative/interactif_Andrew.py
+++ sommative/interactif_Andrew.py
@@ -7,14 +7,6 @@ yes = "ok"
 no = "bye"
 if y == "yes":
   print("then lets begin")
-import random
-import radiant
-correct = random.radiant(1, 10)
-guess = input("Enter your guess: ")
-while guess != correct:
-	if guess > correct:
-		print("You've guessed too high. Try guessing lower.")
-else:
-	print("You've guessed too low. Try guessing higher.")
-guess = int(input("Enter your guess: "))
-print("Congratulations! You've guessed correctly.")
+
+elif y == "no":
+ print("bye")
