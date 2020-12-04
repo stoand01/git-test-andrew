d9b061ce8ec753709bc9e06398022c88cd2499e7
Andrew Stone <stoand01@ecolecatholique.ca>
2020-12-04 16:33:08 +0000

    update

--- sommative/interactif_Andrew.py
+++ sommative/interactif_Andrew.py
@@ -19,6 +19,25 @@ if y == "yes":
     print("close but not quite")
 elif answer == "c":
     print(" yes that is correct ")
-if answer == "b":
-  print("ok question 2")
-  
+if answer == "c":
+  print("good answer question 2")
+  print("who is bruce waynes butler d)alfred dimetime e) afred pennyworth f) alfred nickelback")
+  answer = input().lower()
+  if answer == "d":
+    print("no try again")
+  elif answer == "e":
+    print ("good anakin good")
+  elif answer == "f":
+    print("ha funny")
+  if answer == "e":
+    print ("ok question 3")
+    print("who took andre's porridge a)mother b)mom c)mammy")
+    answer = input().lower()
+  if answer == "a":
+    print("i hope you run into a creeper at night")
+  elif answer == "b":
+    print ("get destroyed pleb")
+  elif answer == "c":
+    print("correct")
+  if answer == "c":
+    print ("you won")
