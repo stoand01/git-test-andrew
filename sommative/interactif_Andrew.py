fe6ea87f274eff8e149ccce0f0296444af625516
Andrew Stone <stoand01@ecolecatholique.ca>
2020-12-04 16:06:17 +0000

    update

--- sommative/interactif_Andrew.py
+++ sommative/interactif_Andrew.py
@@ -9,3 +9,16 @@ if y == "yes":
   print("then lets begin")
 elif y == "no":
  print("bye")
+if y == "yes":
+  print("ok then question 1")
+  print("who is batman a)little wayne b)big wayne c)bruce wayne")
+  answer = input().lower()
+  if answer == "a":
+    print("no thats a talentless rapper")
+  elif answer == "b":
+    print("close but not quite")
+elif answer == "c":
+    print(" yes that is correct ")
+if answer == "b":
+  print("ok question 2")
+  
